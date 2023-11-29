cd221071484dd0aba53b3f1d92915399942f0821
Đậu Đức Quân <bruceparkerblue@gmail.com>
2023-11-29 23:28:40 +0700

    User-info

--- UI Temporary/User_changeinfo/script.js
+++ UI Temporary/User_changeinfo/script.js
@@ -5,62 +5,30 @@ $("document").ready(function () {
   $.get("../modules/footer.html", function (data) {
     $("body").append(data);
   });
+
+  $(".hideedit").css("display", "none"); // Initially set the 'hideedit' section to display: none
+  $("input, select").prop("readonly", true);
+
+  $(".edit").click(function () {
+    $(".showedit").css("display", "none");
+    $(".hideedit").css("display", "block");
+    $("input, select").prop("readonly", false);
+  });
+
+  $(".cancel").click(function () {
+    $(".hideedit").css("display", "none");
+    $(".showedit").css("display", "block");
+    $("input, select").prop("readonly", true);
+  });
+
+  $(".done").click(function () {
+    $(".modal").css("display", "block");
+  });
+
+  $("#btn-done").click(function () {
+    $(".modal").css("display", "none");
+    $(".hideedit").css("display", "none");
+    $(".showedit").css("display", "block");
+    $("input, select").prop("readonly", true);
+  });
 });
-
-// Popup
-var modal = document.getElementById("modal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("btn-done");
-
-// When the user clicks on <span> (x), close the modal
-btn.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-// window.onclick = function (event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
-
-function togglePasswordVisibility() {
-  var passwordInput = document.getElementById("password");
-  var toggleButton = document.querySelector(".toggle-password");
-
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
-    toggleButton.textContent = "Hide";
-  } else {
-    passwordInput.type = "password";
-    toggleButton.textContent = "Show";
-  }
-}
-
-function updateInterface(editMode) {
-  var buttonSection = document.querySelector(".button-section");
-  var modal = document.getElementById("modal");
-
-  if (editMode) {
-    buttonSection.innerHTML = `
-  <button class="done" id="done" style="width: 56px;">Xong</button>
-  <button class="cancel" style="width: 56px;">Hủy</button>
-`;
-    document.getElementById("done").addEventListener("click", function () {
-      modal.style.display = "block";
-    });
-    document.querySelector(".cancel").addEventListener("click", function () {
-      updateInterface(false);
-    });
-    // Add event listener for 'done' button if needed
-  } else {
-    buttonSection.innerHTML = `<button class="edit">Chỉnh sửa thông tin cá nhân</button>`;
-    document.querySelector(".edit").addEventListener("click", function () {
-      updateInterface(true);
-    });
-  }
-}
-
-// Initial setup
-updateInterface(false);
