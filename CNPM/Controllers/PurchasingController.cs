4bcc20460f61d0a0a2115d9a42609efe3f37e565
JenceSon <cumeo2503@gmail.com>
2023-11-29 11:32:23 +0700

    test

---
+++ CNPM/Controllers/PurchasingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CNPM.Controllers
+{
+    public class PurchasingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
