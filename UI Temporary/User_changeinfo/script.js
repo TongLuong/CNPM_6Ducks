250f013a33f50848129ced1fb1db72f6d00d9888
Đậu Đức Quân <bruceparkerblue@gmail.com>
2023-11-29 16:12:01 +0700

    user change info done

--- UI Temporary/User_changeinfo/script.js
+++ UI Temporary/User_changeinfo/script.js
@@ -5,8 +5,30 @@ $("document").ready(function () {
   $.get("../modules/footer.html", function (data) {
     $("body").append(data);
   });
+
+  $(".hideedit").css("display", "none"); // Initially set the 'hideedit' section to display: none
+  $("input, select").prop("readonly", true);
+
+  $(".edit").click(function () {
+    $(".showedit").css("display", "none");
+    $(".hideedit").css("display", "block");
+    $("input, select").prop("readonly", false);
+  });
+
+  $(".cancel").click(function () {
+    $(".hideedit").css("display", "none");
+    $(".showedit").css("display", "block");
+    $("input, select").prop("readonly", true);
+  });
+
+  $(".done").click(function () {
+    $(".modal").css("display", "block");
+  });
+
+  $("#btn-done").click(function () {
+    $(".modal").css("display", "none");
+    $(".hideedit").css("display", "none");
+    $(".showedit").css("display", "block");
+    $("input, select").prop("readonly", true);
+  });
 });
-
-
-
-
