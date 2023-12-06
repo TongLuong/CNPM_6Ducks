1f3f69e15a0babc6c9b3690c19ff59af19d849bd
tungdoanduy <sion080803@gmail.com>
2023-12-06 15:59:52 +0700

    display print log

--- CNPM/wwwroot/User/context/printer-log.js
+++ CNPM/wwwroot/User/context/printer-log.js
@@ -7,3 +7,29 @@ $(document).ready(function () {
   $(".log-item .status").width($(".title .status").width());
   $(".log-table").height(10 * $(".item").height() + 80 + "px");
 });
+
+function showPrintingLogItem(name,time,printer,nopage) {
+    $.get("components/printing-log.html", function (data) {
+        $(".log-table").append(data);
+        var item = $(".log-table .log-item:last-child()");
+        item.find(".name").text(name);
+        item.find("time").text(time);
+        item.find("printer").text(printer);
+        item.find("nopage").text(nopage);
+        item.find("total").text(nopage);
+    }
+    );
+}
+
+function displayPrintingLog(userID) {
+    $.get("PrintingLog/ShowPrintingLog", { "userID": userID },
+        function (response) {
+            for (let i = 0; i < response.number; i++) {
+                showPrintingLogItem(response.filename, response.time, response.printer, response.numberOfPage);
+            }
+        }
+    )
+}
+var urlParams = new URLSearchParams(window.location.search);
+var id = urlParams.get('id');
+displayPrintingLog(id);
