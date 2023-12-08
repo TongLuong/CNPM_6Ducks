6680bac615f27f1372c57e33ce53c2f9599c33c5
tungdoanduy <sion080803@gmail.com>
2023-12-08 22:25:40 +0700

    save progress

--- CNPM/Controllers/AdminManageUser.cs
+++ CNPM/Controllers/AdminManageUser.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace CNPM.Controllers
 {
@@ -7,6 +9,49 @@ namespace CNPM.Controllers
         public IActionResult Index()
         {
             return View("/Views/Admin/manage-user.cshtml");
+        }
+
+        SqlConnection conn;
+
+        public AdminManageUser()
+        {
+            //conn = new SqlConnection(connectionString);
+            conn = new SqlConnection(ConnectionString.sqlConnectionString);
+        }
+
+        public JsonResult ShowUser()
+        {
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+
+            SqlCommand cmd = new SqlCommand("select [name], [user_id], faculty, pageLeft from [User])", conn);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            int num = 0;
+            List<string> names = new List<string>();
+            List<string> userIDs = new List<string>();
+            List<string> faculties = new List<string>();
+            List<string> pagesLeft = new List<string>();
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    num++;
+                    names.Add(dr.GetString(0));
+                    userIDs.Add(dr.GetString(1));
+                    faculties.Add(dr.GetString(2));
+                    pagesLeft.Add(dr.GetInt32(3).ToString());
+                }
+            }
+
+            conn.Close();
+
+            return new JsonResult
+            (
+                new { number = num, name = names, userID = userIDs, faculty = faculties, pageLeft = pagesLeft }
+            );
+
         }
     }
 }
--- CNPM/wwwroot/Admin/js/admin-manage-user.js
+++ CNPM/wwwroot/Admin/js/admin-manage-user.js
@@ -2,4 +2,39 @@ $(document).ready(function () {
   $("#myDropdown div").click(function() {
     $(".dropdown").has(this).find("span").text($(this).text());
   });
+
+    alert("Hieuga");
+
+    function show_user_item(stt, name, userID, faculty, pageLeft) {
+        $(".filter-record").append($("<div></div>").addClass("record").text(stt));
+        $(".filter-record").append($("<div></div>").addClass("record").text(name));
+        $(".filter-record").append($("<div></div>").addClass("record").text(userID));
+        $(".filter-record").append($("<div></div>").addClass("record").text(faculty));
+        $(".filter-record").append($("<div></div>").addClass("record").text(pageLeft));
+
+
+        alert("hakf");
+        // $.get("components/user_admin.html", function (data) {
+        //     $(".filter-record").append(data);
+        //     var item = $(".filter-record .user-item:last-child()");
+        //     item.find("#stt").text(stt);
+        //     item.find("#name").text(name);
+        //     item.find("#user-id").text(userID);
+        //     item.find("#faculty").text(faculty);
+        //     item.find("#page-left").text(pageLeft);
+        // }
+        // );
+    }
+
+    function display_user() {
+        $.get("AdminManageUser/ShowUser",
+            function (response) {
+                for (let i = 0; i < response.number; i++) {
+                    show_user_item(i.toString(), response.name[i], response.userID[i], response.faculty[i], response.pageLeft[i]);
+                }
+            }
+        )
+    }
+
+    display_user();
 });
