c43c6e2ddd34cbb663fba1b8335b38d1c8a7b437
tungdoanduy <sion080803@gmail.com>
2023-12-09 20:42:26 +0700

    Update AdminManageSystem.cs

--- CNPM/Controllers/AdminManageSystem.cs
+++ CNPM/Controllers/AdminManageSystem.cs
@@ -72,5 +72,66 @@ namespace CNPM.Controllers
             }
             conn.Close();
         }
+
+        public int LoadTotalPrint(int year)
+        {
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM total_print(@year)", conn);
+
+            cmd.Parameters.AddWithValue("@year", year);
+
+            int res = (int)cmd.ExecuteScalar();
+
+            conn.Close();
+
+            return res;
+        }
+
+        public int LoadTotalPageA4(int year, int month)
+        {
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM total_page_A4(@year,@mm)", conn);
+
+            cmd.Parameters.AddWithValue("@year", year);
+            cmd.Parameters.AddWithValue("@mm", month);
+
+            int res = (int)cmd.ExecuteScalar();
+
+            conn.Close();
+
+            return res;
+        }
+
+        public JsonResult StatTotalPage(int year)
+        {
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM total_print(@year)", conn);
+
+            cmd.Parameters.AddWithValue("@year", year);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            List<string> months = new List<string>();
+            List<string> total_pages = new List<string>();
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    months.Add(dr.GetInt32(0).ToString());
+                    total_pages.Add(dr.GetInt32(0).ToString());
+                }
+            }
+
+            conn.Close();
+
+            return new JsonResult(
+                new { month = months, total_page = total_pages });
+        }
     }
 }
