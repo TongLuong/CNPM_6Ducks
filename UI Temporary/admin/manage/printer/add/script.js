2f610654b27d27ccb8edec6dd1570b89918b390b
TongLuong <tong.luong203@hcmut.edu.vn>
2023-12-12 17:28:44 +0700

    delete UI temporary

--- UI Temporary/admin/manage/printer/add/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $(".choose .list").height($(".property-list").height())
-
-  $(".printer-list .item").click(function() {
-    window.location.href = "../remove/";
-  });
-});
--- UI Temporary/admin/manage/user/script.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-  $("#myDropdown div").click(function() {
-    $(".dropdown").has(this).find("span").text($(this).text());
-  });
-});
